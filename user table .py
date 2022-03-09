ba10852a2207232e360894f88b5ce948e0fbbe04
ShaliniSingh21 <91829539+ShaliniSingh21@users.noreply.github.com>
2022-03-09 16:04:34 +0530

    Add files via upload
    done

---
+++ user table .py
@@ -0,0 +1,18 @@
+no=int(input('enter the no'))
+dic={}
+dict={}
+for i in range (1,11):
+    dic[i]=no*i
+    #dict[no]=dic
+print(dic,end=" ")
+
+
+# no=int(input('enter the no'))
+# dic={}
+# dict={}
+# for i in range (1,11):
+#     dic[i]=no*i
+#     dict[no]=dic
+# print(dict)
+
+
